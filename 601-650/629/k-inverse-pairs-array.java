69c3b8e4f0e9b5a54b182390c9293beea56c2739
pivotone <37400245+pivotone@users.noreply.github.com>
2021-11-19 20:32:38 +0800

    Update k-inverse-pairs-array.java

--- 601-650/629/k-inverse-pairs-array.java
+++ 601-650/629/k-inverse-pairs-array.java
@@ -25,21 +25,21 @@ class Solution {
     }
   
   // 快
-    // final int mod = 1000000007;
+    final int mod = 1000000007;
 
-    // public int kInversePairs(int n, int k) {
-    //     long[][] dp = new long[n + 1][k + 1];
-    //     dp[0][0] = 1;
+    public int kInversePairs(int n, int k) {
+        long[][] dp = new long[n + 1][k + 1];
+        dp[0][0] = 1;
 
-    //     for (int i = 1; i <= n; ++ i) {
-    //         long sum = 0;
-    //         for (int j = 0; j <= k; ++ j) {
-    //             sum += dp[i - 1][j];
-    //             if (j + 1 > i) sum -= dp[i - 1][j - i];
-    //             dp[i][j] = (dp[i][j] + sum) % mod;
-    //         }
-    //     }
+        for (int i = 1; i <= n; ++ i) {
+            long sum = 0;
+            for (int j = 0; j <= k; ++ j) {
+                sum += dp[i - 1][j];
+                if (j + 1 > i) sum -= dp[i - 1][j - i];
+                dp[i][j] = (dp[i][j] + sum) % mod;
+            }
+        }
 
-    //     return (int) (dp[n][k]);
-    // }
+        return (int) (dp[n][k]);
+    }
 }
