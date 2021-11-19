a2dd253a3586e8d4d810fa4a0137de0aca1eb864
pivotone <37400245+pivotone@users.noreply.github.com>
2021-11-19 19:51:18 +0800

    Update TwoSum.java

---
+++ 1-50/1/TwoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map map = new HashMap<Integer, Integer>();
+        int[] result = new int[2];
+        for(int i = 0; i < nums.length; ++i) {
+            if(map.containsKey(nums[i])) {
+                result[0] = i;
+                result[1] = (int)map.get(nums[i]);
+                break;
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+        return result;
+    }
+}
