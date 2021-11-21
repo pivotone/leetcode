4474b33b030130b2814380c8bb48461bebfeff6f
pivotone <37400245+pivotone@users.noreply.github.com>
2021-11-21 18:04:32 +0800

    Create symmetric-tree.java

---
+++ 101-150/101/symmetric-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// 求证左右子树是否相等，递归思想
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isEqual(root.left, root.right);
+    }
+
+    public boolean isEqual(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        return left.val == right.val && isEqual(left.left, right.right) && isEqual(left.right, right.left);
+    }
+}
